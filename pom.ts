2cab11a6a16dc86afbf6300c3c84162b5a2850a6
a.kladiev <a.kladiev@it-alnc.ru>
2024-06-01 12:53:49 +0300

    init playwright project

---
+++ pom.ts
@@ -0,0 +1,13 @@
+import { test as base } from '@playwright/test';
+
+/**
+ * This adds a todoPage fixture which has access to the page instance
+ * @see https://playwright.dev/docs/test-fixtures
+ */
+export const test = base.extend<{ todoPage: TodoPage }>({
+  todoPage: async ({ page }, use) => {
+    await use(new TodoPage(page));
+  },
+});
+
+export const expect = test.expect;

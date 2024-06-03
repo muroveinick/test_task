b37868e4573963a58e35c195266e5fd749911786
muroveinick <62598654+muroveinick@users.noreply.github.com>
2024-06-03 03:07:18 +0300

    undef check in compareObjects

--- src/functions.ts
+++ src/functions.ts
@@ -1,6 +1,10 @@
 export const delay = (ms: number) => new Promise((res) => setTimeout(res, ms));
 
 export const compareObjects = (verifiable_obj: any, mask_obj: any, soft_comparation = true) => {
+  if (!verifiable_obj || !mask_obj) {
+    return false;
+  }
+
   let final_decision = true;
   for (const key in mask_obj) {
     if (key in verifiable_obj) {
